dd06c50c415311412d154162709181386d102326
xiaowei <shenzhoudance@gmail.com>
2018-10-23 16:58:58 +0800

    add commnets

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,5 @@
 class Submission < ApplicationRecord
   belongs_to :user
   acts_as_votable
+  has_many :commnets
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023083402) do
+ActiveRecord::Schema.define(version: 20181023084825) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "submission_id"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["submission_id"], name: "index_comments_on_submission_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
